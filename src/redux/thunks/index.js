47286ed34e792c472e9defc6451952021aaa4b97
Carlos Pajares <c.pajares@hotmail.com>
2021-11-01 18:04:06 +0100

    created loadTasksThunks at thunks

--- src/redux/thunks/index.js
+++ src/redux/thunks/index.js
@@ -1,3 +1,9 @@
-const loadTasksThunks = () => {
-  return async () => {};
+import { loadTasksAction } from "../actions/actionCreators";
+
+export const loadTasksThunks = () => {
+  return async (dispatch) => {
+    const response = await fetch("https://wk06todo.herokuapp.com/todo");
+    const tasks = await response.json();
+    dispatch(loadTasksAction(tasks));
+  };
 };
